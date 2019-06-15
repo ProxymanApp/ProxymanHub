3352fe5d6d4e72c757a76550a9b312d5983777b4
Nghia Tran <vinhnghiatran@gmail.com>
2019-06-15 08:54:44 +0700

    Initial Commit

---
+++ ProxymanHub/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ProxymanHub
+//
+//  Created by Nghia Tran on 6/15/19.
+//  Copyright © 2019 Nghia Tran. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
